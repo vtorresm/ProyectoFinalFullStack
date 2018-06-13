54887f5c144bd296aeb085592f1472f2398b3bf9
Victor Torres <vatorresm88@gmail.com>
2018-06-13 00:17:12 -0500

    Ruta base de datos

--- backend/database.ts
+++ backend/database.ts
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const URI = 'mongodb://localhost/crud-ferreteria';
+//const URI = 'mongodb://userDB:userDB123@ds245210.mlab.com:45210/ferreteria';
 
 mongoose.connect(URI)
     .then(db => console.log('db esta conectado'))
